33312978ffe9a94aeeafa81950b81e9e6a085f3f
PalmDevs <palmpasuthorn@gmail.com>
2023-12-02 20:40:47 +0700

    feat(2023/utils/inputs): add an option to look in parent dir

--- 2023/utils/input.ts
+++ 2023/utils/input.ts
@@ -3,15 +3,16 @@ import { readFile } from 'fs/promises'
 
 import { streamToString } from './stream'
 
-export async function getRawInput() {
+export async function getRawInput(attemptToLookInParentDir = false) {
     if (existsSync('./input.txt')) return await readFile('./input.txt', 'utf-8')
+    if (attemptToLookInParentDir && existsSync('../input.txt')) return await readFile('../input.txt', 'utf-8')
 
     const stdinContents = await streamToString(process.stdin)
     if (stdinContents.length) return stdinContents
-    
+
     throw new Error('No input was provided')
 }
 
-export async function getInput() {
-    return (await getRawInput()).trim().replace(/\r\n/g, '\n')
+export async function getInput(attemptToLookInParentDir = false) {
+    return (await getRawInput(attemptToLookInParentDir)).trim().replace(/\r\n/g, '\n')
 }
