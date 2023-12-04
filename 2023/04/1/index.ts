37f5ef0326841828ad1262dc30238613097eeb54
Palm <palmpasuthorn@gmail.com>
2023-12-04 18:20:13 +0000

    chore(2023/04/1): apply biome fixes

--- 2023/04/1/index.ts
+++ 2023/04/1/index.ts
@@ -3,19 +3,28 @@ import { getInput } from '../../utils/input'
 const input = await getInput()
 const lines = input.split('\n')
 
-const sum = lines.map(line => {
-    const [,  cards] = line.split(': ')
-    const [winningInts, ownedInts] =
-        cards.split('|').map(s => new Set(s.trim().split(/  | /).map(Number)))
-    
-    let count = 0;
-    for (const int of ownedInts) {
-        if (winningInts.has(int)) count++
-    }
+const sum = lines
+    .map(line => {
+        const [, cards] = line.split(': ')
+        const [winningInts, ownedInts] = cards.split('|').map(
+            s =>
+                new Set(
+                    s
+                        .trim()
+                        .split(/ {1,2}/)
+                        .map(Number),
+                ),
+        )
 
-    return count
-}).reduce((pv, cv) => {
-    return pv + (cv < 1 ? 0 : 2**(cv-1))
-}, 0)
+        let count = 0
+        for (const int of ownedInts) {
+            if (winningInts.has(int)) count++
+        }
+
+        return count
+    })
+    .reduce((pv, cv) => {
+        return pv + (cv < 1 ? 0 : 2 ** (cv - 1))
+    }, 0)
 
 console.log(sum)
