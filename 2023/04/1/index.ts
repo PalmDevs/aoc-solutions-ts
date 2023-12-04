b146e56e3a829dd0f9b2776fe7ae78e0eeea6ab9
Palm <palmpasuthorn@gmail.com>
2023-12-04 18:15:28 +0000

    feat(2023/04): add part 1 solution

---
+++ 2023/04/1/index.ts
@@ -0,0 +1,21 @@
+import { getInput } from '../../utils/input'
+
+const input = await getInput()
+const lines = input.split('\n')
+
+const sum = lines.map(line => {
+    const [,  cards] = line.split(': ')
+    const [winningInts, ownedInts] =
+        cards.split('|').map(s => new Set(s.trim().split(/  | /).map(Number)))
+    
+    let count = 0;
+    for (const int of ownedInts) {
+        if (winningInts.has(int)) count++
+    }
+
+    return count
+}).reduce((pv, cv) => {
+    return pv + (cv < 1 ? 0 : 2**(cv-1))
+}, 0)
+
+console.log(sum)
