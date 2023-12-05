de1a37581c27cc425da0a430d98905092718b639
PalmDevs <palmpasuthorn@gmail.com>
2023-12-06 00:30:38 +0700

    fix(2023/utils): no input resulting in freeze

--- 2023/utils/stream.ts
+++ 2023/utils/stream.ts
@@ -1,7 +1,10 @@
-export function streamToString(stream: NodeJS.ReadableStream) {
+import { Readable } from 'stream'
+
+export function streamToString(stream: Readable) {
     const chunks: string[] = []
     return new Promise<string>((resolve, reject) => {
-        stream.on('data', chunk => chunks.push(chunk))
+        let timeout = setInterval(() => resolve(chunks.join('')), 1000)
+        stream.on('data', chunk => (chunks.push(chunk), clearTimeout(timeout), setInterval(() => resolve(chunks.join('')), 1000)))
         stream.on('end', () => resolve(chunks.join('')))
         stream.on('error', reject)
     })
