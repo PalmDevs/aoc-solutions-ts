e1ee487d300afc1d89090e88a2a09c6fed6e7cf4
PalmDevs <palmpasuthorn@gmail.com>
2023-12-06 02:39:01 +0700

    chore(2023/utils/stream): apply biome fixes

--- 2023/utils/stream.ts
+++ 2023/utils/stream.ts
@@ -3,8 +3,12 @@ import { Readable } from 'stream'
 export function streamToString(stream: Readable) {
     const chunks: string[] = []
     return new Promise<string>((resolve, reject) => {
-        let timeout = setInterval(() => resolve(chunks.join('')), 1000)
-        stream.on('data', chunk => (chunks.push(chunk), clearTimeout(timeout), setInterval(() => resolve(chunks.join('')), 1000)))
+        const timeout = setInterval(() => resolve(chunks.join('')), 1000)
+        stream.on('data', chunk => {
+            chunks.push(chunk)
+            clearTimeout(timeout)
+            setInterval(() => resolve(chunks.join('')), 1000)
+        })
         stream.on('end', () => resolve(chunks.join('')))
         stream.on('error', reject)
     })
