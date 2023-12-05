8c9136a35885fd2e2afec1850bde795d7e471f54
PalmDevs <palmpasuthorn@gmail.com>
2023-12-06 01:25:26 +0700

    fix(2023/03/1): fix matching issues and dupe detector system

--- 2023/03/1/index.ts
+++ 2023/03/1/index.ts
@@ -1,21 +1,27 @@
 import { getInput } from '../../utils/input'
 
-const input = await getInput()
+const input = await getInput(true)
 const lines = input.split('\n')
 const positionToFindSymbols = getIntegerHitboxRangesPerLine(lines)
 
 let sum = 0
+const indexesInitedForAlreadyMatchedFIs = new Set<number>()
 const alreadyMatchedFirstIndexes: Array<number[]> = []
 
 for (let i = 0; i < lines.length; i++) {
     const line = lines[i]
     const affectedColumnsRanges = positionToFindSymbols[i]
-    alreadyMatchedFirstIndexes[i] = []
     for (let j = 0; j < affectedColumnsRanges.length; j++) {
-        const [data, firstIndex, x1, x2] = affectedColumnsRanges[j]
+        const [data, lineIndex, firstIndex, x1, x2] = affectedColumnsRanges[j]
+
+        if (!indexesInitedForAlreadyMatchedFIs.has(lineIndex)) {
+            alreadyMatchedFirstIndexes[lineIndex] = []
+            indexesInitedForAlreadyMatchedFIs.add(lineIndex)
+        }
+        
         for (let k = x1; k <= x2; k++) {
-            if (!isIntegerChar(line[k]) && line[k] !== '.' && !alreadyMatchedFirstIndexes[i].includes(firstIndex)) {
-                alreadyMatchedFirstIndexes[i].push(firstIndex)
+            if (!isIntegerChar(line[k]) && line[k] !== '.' && !alreadyMatchedFirstIndexes[lineIndex].includes(firstIndex)) {
+                alreadyMatchedFirstIndexes[lineIndex].push(firstIndex)
                 sum += data
             }
         }
@@ -25,8 +31,8 @@ for (let i = 0; i < lines.length; i++) {
 console.log(sum)
 
 function getIntegerHitboxRangesPerLine(lines: string[]) {
-    // 2D array (array of lines) of [Value, FirstIndex, X1, X2]
-    const ranges: Array<Array<[number, number, number, number]>> = []
+    // 2D array (array of lines) of [Value, LineIndex, FirstIndex, X1, X2]
+    const ranges: Array<Array<[number, number, number, number, number]>> = []
 
     for (let i = 0; i < lines.length; i++) {
         const line = lines[i]
@@ -39,8 +45,8 @@ function getIntegerHitboxRangesPerLine(lines: string[]) {
         let prevMatchWasInt = false
         let firstIntIndexInGroup = -1
 
-        for (let j = 0; j < lines.length; j++) {
-            const charIsInt = isIntegerChar(line[j])
+        for (let j = 0; j <= line.length; j++) {
+            const charIsInt = isIntegerChar(line[j] ?? '.')
 
             // This means we're in the same number group, so we can just ignore
             if (prevMatchWasInt) {
@@ -53,10 +59,11 @@ function getIntegerHitboxRangesPerLine(lines: string[]) {
 
                 const data = [
                     Number(line.slice(firstIntIndexInGroup, j)),
+                    i,
                     firstIntIndexInGroup,
                     bindedClamp(firstIntIndexInGroup - 1),
-                    bindedClamp(j + 1),
-                ] as [number, number, number, number]
+                    bindedClamp(j),
+                ] as [number, number, number, number, number]
 
                 ranges[i].push(data)
                 if (i > 0) ranges[i - 1].push(data)
