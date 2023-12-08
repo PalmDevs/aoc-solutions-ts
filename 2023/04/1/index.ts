0e66d471322f79f75a6393c125b92f7c8bb964df
Palm <palmpasuthorn@gmail.com>
2023-12-08 08:54:30 +0000

    feat(2023/04): add shared components

--- 2023/04/1/index.ts
+++ 2023/04/1/index.ts
@@ -1,30 +1,7 @@
 import { getInput } from '../../utils/input'
+import { getCardsWinnings } from '../shared'
 
 const input = await getInput()
 const lines = input.split('\n')
 
-const sum = lines
-    .map(line => {
-        const [, cards] = line.split(': ')
-        const [winningInts, ownedInts] = cards.split('|').map(
-            s =>
-                new Set(
-                    s
-                        .trim()
-                        .split(/ {1,2}/)
-                        .map(Number),
-                ),
-        )
-
-        let count = 0
-        for (const int of ownedInts) {
-            if (winningInts.has(int)) count++
-        }
-
-        return count
-    })
-    .reduce((pv, cv) => {
-        return pv + (cv < 1 ? 0 : 2 ** (cv - 1))
-    }, 0)
-
-console.log(sum)
+console.log(getCardsWinnings(lines).reduce((pv, cv) => pv + (cv < 1 ? 0 : 2 ** (cv - 1)), 0))
